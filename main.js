2f310bf3b815e282c81b5d9979bd47f9ece015c2
Will-Eves <williameves860@gmail.com>
2022-01-28 22:05:04 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,126 @@
+var items = [];
+var recipies = [];
+
+var inventory = [];
+
+var gamedata;
+
+function addItem(name){
+    items[name] = [name];
+}
+
+function addRecipie(name, items){
+    recipies.push([name, items]);
+}
+
+function craft(items){
+    var length = recipies.length;
+    var name = "None";
+    for(var i = 0; i < length; i++){
+        if(recipies[i][1].length == items.length){
+            var len = recipies[i][1].length;
+            var craft = false;
+            for(var j = 0; j < len; j++){
+                craft = true;
+                for(var a = 0; a < len; a++){
+                    if(recipies[i][1][a] == items[j]){
+                        craft = false;
+                    }
+                }
+            }
+            craft = (recipies[i][1].sort().join(',') === items.sort().join(','));
+            if(craft){
+                name = recipies[i][0];
+                if(inventory[name] == null){
+                    inventory[name] = 0;
+                }
+                inventory[name]++;
+                break;
+            }
+        }
+    }
+    return name;
+}
+
+var inventoryElement;
+var craftElement;
+
+window.onload = function(){
+    addItem("air");
+    addItem("water");
+    addItem("fire");
+    addItem("earth");
+
+    inventory["air"] = 1;
+    inventory["water"] = 1;
+    inventory["fire"] = 1;
+    inventory["earth"] = 1;
+    
+    addRecipie("mud", ["water", "earth"]);
+    addRecipie("dust", ["fire", "earth"]);
+    addRecipie("sand", ["air", "earth"]);
+    addRecipie("hill", ["earth", "earth"]);
+
+    addRecipie("energy", ["water", "fire"]);
+    addRecipie("plasma", ["air", "fire"]);
+    addRecipie("inferno", ["fire", "fire"]);
+
+    addRecipie("cloud", ["water", "air"]);
+    addRecipie("wind", ["air", "air"]);
+
+    addRecipie("pond", ["water", "water"]);
+    
+    inventoryElement = document.getElementById("inventory");
+    craftElement = document.getElementById("craft");
+
+    console.log(document.getElementById("gamedata").getAttribute('data-id'));
+}
+
+//Drag Handling
+function allowDrop(e){
+    e.preventDefault();
+}
+
+var data;
+
+function drag(e){
+    data = e.target;
+}
+
+function drop(e){
+    e.preventDefault();
+    var target = e.target;
+    if(e.target.getAttribute("name") == "item"){
+        target = e.target.parentElement;
+    }
+    target.appendChild(data);
+    if(target.id == "craft"){
+        var craftElements = [];
+        for(var i = 0; i < target.childNodes.length; i++){
+            craftElements.push(target.childNodes[i]);
+        }
+        var names = [];
+        for(var i = 0; i < craftElements.length; i++){
+            names.push(craftElements[i].id);
+        }
+        var c = craft(names);
+        if(c != "None"){
+            if(inventory[c] == 1){
+                var element = document.createElement("div");
+                element.id = c;
+                element.classList.add("image");
+                element.draggable = true;
+                element.setAttribute("ondragstart", "drag(event)");
+                element.setAttribute("name", "item"); 
+                element.innerText = c;
+                inventoryElement.appendChild(element);
+            }
+            
+            var len = craftElements.length;
+            for(var i = 0; i < len; i++){
+                inventoryElement.appendChild(craftElements[i]);
+                i++;
+            }
+        }
+    }
+}
